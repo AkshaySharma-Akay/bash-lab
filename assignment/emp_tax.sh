4a0264fc6d0f21f9fdc8475cb74285c3ef2d6aa7
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2018-08-29 00:54:38 +0530

    Added shell program for caculate tax

---
+++ assignment/emp_tax.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+a=25000
+while read emp_name eid salary
+do
+    if [ $salary -le $a ]
+    then
+            tax=0
+    elif [ $salary -gt 25000 ] && [ $salary -le 50000 ]
+    then
+        tax=`expr $salary / 10`
+    elif [ $salary -gt 50000 ] && [ $salary -le 100000 ]
+    then
+        tax=`expr $salary / 5`
+    else
+        tax=`expr $salary \* 3`
+        tax=`expr $tax / 10`
+    fi
+    echo "$emp_name : Tax = $tax "
+done
