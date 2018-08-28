c2837b9028fcf0b3e1dbcb0e00d256c3ba2fb6f1
AkshaySharma-Akay <akshaysharmaak2@gmail.com>
2018-08-29 01:13:38 +0530

    Merged the average and the grade script:

--- assignment/get_avg.sh
+++ assignment/get_avg.sh
@@ -4,8 +4,22 @@ while read stu_roll_no stu_marks stu_name
 do
     sum=`expr $sum + $stu_marks`
     count=`expr $count + 1` 
-    if [ $stu_marks -lt 50 ]; then
+    if [ $stu_marks -lt 40 ]; then
         countfail=`expr $countfail + 1`;
+    fi
+    echo -ne "$stu_name ( $stu_marks ): "
+    if [ $stu_marks -ge 80 ]; then
+        echo  "A"
+    elif [ $stu_marks -ge 70 ] && [ $stu_marks -lt 80 ]; then
+        echo "B "
+    elif [ $stu_marks -ge 60 ] && [ $stu_marks -lt 70 ]; then
+        echo "C"
+    elif [ $stu_marks -ge 50 ] && [ $stu_marks -lt 60 ]; then
+        echo "D"
+    elif [ $stu_marks -ge 40 ] && [ $stu_marks -lt 50  ]; then
+        echo "E"
+    else
+        echo "F"
     fi
 done
 echo "The Average is $(($sum/$count))"
